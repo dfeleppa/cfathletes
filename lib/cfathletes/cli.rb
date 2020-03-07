773afd0e9c4d1afce5dd1d9bd1f75733a3aa8273
dfeleppa <dfeleppa@gmail.com>
2020-03-07 18:48:54 -0500

    add cli notes

--- lib/cfathletes/cli.rb
+++ lib/cfathletes/cli.rb
@@ -1,8 +1,17 @@
 class Cfathletes
 
+    #start should greet user and give description
+    #get data from api
+    #create new custom objs
+    #all inside start method
+    def start
+        puts "Welcome to the CrossFit Athlete CLI!",
+        "This CLI parses data from the 2018 Crossfit Open Leaderboard.",
+        "<Next Line>"
 
+    end
     
-    def initialize
-        puts "Welcome"
-     end
+    
 end
+
+Cfathletes.new.start
